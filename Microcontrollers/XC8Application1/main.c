823477437a8f1ca89793a5c3703c328795da3f94
Guilliam Lutz <guilliamlutz@outlook.com>
2021-02-10 09:27:42 +0100

    Opdracht B.5

---
+++ Microcontrollers/XC8Application1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/10/2021 9:26:01 AM
+ *  Author: User
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
