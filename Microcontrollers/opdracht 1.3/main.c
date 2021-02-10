8a230bd1ee84684104b1e8ef45f1e1a1da7a74ff
Sem van der Hoeven <semmer99@gmail.com>
2021-02-10 09:40:47 +0100

    add 1.3

--- Microcontrollers/opdracht 1.3/main.c
+++ Microcontrollers/opdracht 1.3/main.c
@@ -7,12 +7,28 @@
 
 #include <avr/io.h>
 
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRD = 0b11111111; // d is output
+	DDRC = 0b00000000; // c is all input
+    
     while (1) 
     {
+		if (PINC & 0b00000001) {
+			
+			PORTD = 0b01000000;
+			wait(1000);
+			PORTD = 0b00000000;
+			wait(1000);
+		}
     }
 }
 
