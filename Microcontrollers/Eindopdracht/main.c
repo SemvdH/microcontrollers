9594f0e367bc4911b015df8dc704ce7ae9a5985c
Sem van der Hoeven <semmer99@gmail.com>
2021-03-31 09:47:08 +0200

    added skeleton for eindopdracht

--- Microcontrollers/Eindopdracht/main.c
+++ Microcontrollers/Eindopdracht/main.c
@@ -5,14 +5,40 @@
  * Author : Sem
  */ 
 
-#include <avr/io.h>
+#define F_CPU 10e6
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "lcd_control.h"
+#include "ultrasonic_sensor.h"
+
+
+ISR(INT0_vect)
+{
+	ultrasonic_handle_interrupt();
+}
 
 int main(void)
 {
     /* Replace with your application code */
+	ultrasonic_init();
+	init_4bits_mode();
+	
+	_delay_ms(10);
+	
+	lcd_clear();
     while (1) 
     {
+		// TODO change to use the stepper motor
+		ultrasonic_send_pulse(); 
+		
+		lcd_clear();
+		lcd_write_int(ultrasonic_get_timer_dist());
+		
+		
+		wait_ms(100);
     }
 }
 
