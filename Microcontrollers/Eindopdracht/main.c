7051c8c7c9e1e623d113ef93ec6decdc92a9f0a7
Sem van der Hoeven <semmer99@gmail.com>
2021-03-31 09:42:35 +0200

    [ADD] eindopdracht project

---
+++ Microcontrollers/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 31-3-2021 09:42:17
+ * Author : Sem
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
